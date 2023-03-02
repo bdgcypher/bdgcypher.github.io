34232110300ef833446a1fd6d049440f5471cc40
BenjaminGoddard <bdgcypher@gmail.com>
2023-03-02 10:47:59 -0700

    changed tailwind colors to default black and white

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,6 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        black: '#1E1F1F',
-        white: '#FCFCFC',
         snow: '#E8ECEE',
         onyx: '#3C3E3E',
         raisin: '#242E33',
