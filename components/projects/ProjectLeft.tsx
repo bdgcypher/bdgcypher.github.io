21e74f6b5e82ade0e067e69a915f8af27545a978
Benjamin Goddard <79329211+bdgcypher@users.noreply.github.com>
2024-07-25 14:49:06 -0600

    Update ProjectLeft.tsx

--- components/projects/ProjectLeft.tsx
+++ components/projects/ProjectLeft.tsx
@@ -13,9 +13,9 @@ export default function ProjectLeft(
         <div className="px-4 mx-auto mb-16 lg:mb-20 sm:max-w-xl md:max-w-full lg:max-w-screen-2xl md:px-24 lg:px-8">
             <div className="max-w-2xl mb-10 ml-0 lg:ml-16 text-left md:mb-12">
                 <div className="to-animate from-left flex flex-row max-w-lg mb-6 font-sans text-3xl transition ease-in-out duration-300 font-bold leading-none tracking-tight text-black dark:text-white sm:text-4xl">
-                    <h1>
+                    <h2>
                         {title}
-                    </h1>
+                    </h2>
                     <span className="mx-8" />
                     <a target="_blank" aria-label="website link" href={siteLink} >
                         <div className="flex flex-row max-w-lg mb-6 font-sans text-3xl transition ease-in-out duration-300 hover:-translate-y-2 font-bold leading-none tracking-tight text-black dark:text-white hover:text-primary dark:hover:text-primary sm:text-4xl cursor-pointer">
@@ -45,9 +45,9 @@ export default function ProjectLeft(
             <div className="grid max-w-screen-xl gap-8 lg:grid-cols-2 sm:mx-auto">
                 <div className="to-animate from-left flex flex-col">
                     <div className="pb-4 mb-4">
-                        <h6 className="mb-2 font-semibold text-3xl text-black dark:text-white border-b">
+                        <h3 className="mb-2 font-semibold text-3xl text-black dark:text-white border-b">
                             {header}
-                        </h6>
+                        </h3>
                         <p className="text-lg md:text-xl text-gray-700 dark:text-gray-400">
                             {longDescription}
                         </p>
