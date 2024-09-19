75cf09a899176afdcf39d2e3915920875db5210b
Benjamin Goddard <79329211+bdgcypher@users.noreply.github.com>
2024-09-19 12:07:32 -0600

    Fixing header hierarchy in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -63,7 +63,7 @@ export default function Footer() {
         <footer className="bg-white dark:bg-black">
             <TypeIt options={{ speed: 10, waitUntilVisible: true }}>
                 <div className="h-20 text-center mx-auto w-3/4 md:w-full">
-                    <h3 className="text-black dark:text-white text-3xl md:text-4xl lg:text-5xl">Each day is a chance to <span className="text-primary">Level Up</span>!</h3>
+                    <p className="text-black dark:text-white text-3xl md:text-4xl lg:text-5xl">Each day is a chance to <span className="text-primary">Level Up</span>!</p>
                     <div className="mt-2 text-gray-400 text-md md:text-xl">
                         <p>&#47;&#47; So make it count.</p>
                     </div>
