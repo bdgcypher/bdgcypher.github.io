1adb0a852153c08152206a1972846492c9f70204
Benjamin Goddard <bdgcypher@gmail.com>
2024-11-05 09:11:05 -0700

    Updated login info for "Axis" project demo

--- components/index/Discover.tsx
+++ components/index/Discover.tsx
@@ -65,7 +65,7 @@ export default function Discover() {
                             <div className="absolute inset-0 px-6 py-4 transition-opacity duration-200 bg-black bg-opacity-95 opacity-0 hover:opacity-100">
                                 <p className="mb-4 text-3xl font-bold text-gray-100">Axis</p>
                                 <p className="text-xl tracking-wide text-gray-300">
-                                    A Discord-esque chat platform for me and my friends.
+                                    A Discord-esque chat platform for me and my friends.<br /> <br/>(Username and password: "guest")
                                 </p>
                             </div>
                         </div>
