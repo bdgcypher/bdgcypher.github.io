0fa1a29a80d36daef47c43ffd03965f929f39f7c
Benjamin Goddard <bdgcypher@gmail.com>
2024-11-14 10:32:29 -0700

    Fixed back button link

--- pages/tip.tsx
+++ pages/tip.tsx
@@ -68,7 +68,7 @@ export default function TiP() {
 
             <div>
                 <div id="home" className="pt-4 pl-4 bg-white dark:bg-black">
-                    <a href="/discover" aria-label="back button" className="text-primary hover:text-secondary text-4xl transition ease-in-out duration-300">
+                    <a href="/projects" aria-label="back button to projects page" className="text-primary hover:text-secondary text-4xl transition ease-in-out duration-300">
                         <BiArrowBack />
                     </a>
                     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
